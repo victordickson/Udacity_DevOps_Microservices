c328d7d8c6b1aa8679a591d51cd706b63fb0db46
EC2 Default User <ec2-user@ip-172-31-23-92.ec2.internal>
2022-07-03 23:37:00 +0000

    first commit

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="vickcode/udacityproj4:v1.0.0"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacityproj4\
+    --image=$dockerpath\
+    --port=80 --labels app=udacityproj4
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward udacityproj4 8000:80
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="vickcode/udacityproj4:v1.0.0"
 
 # Step 2:  
 # Authenticate & tag
+docker login
+docker tag udacityproj4 ${dockerpath}
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push ${dockerpath}
